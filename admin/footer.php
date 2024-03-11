739f70d69a13868fdf663b20a7c87b109f2884b8
tuanvu08 <113953351+tuanvu08@users.noreply.github.com>
2024-03-11 07:58:32 +0700

    add admin

---
+++ admin/footer.php
@@ -0,0 +1,72 @@
+<section class="myfooter bg-dark text-white py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <h6>VĂN PHÒNG GIAO DỊCH</h6>
+                <ul class="list-menu">
+                    <li class="li_menu"><a href="#" title="trang chủ">Hà Nội</a></li>
+                    <li class="li_menu"><a href="#" title="trang chủ">TP.Hồ Chí Minh</a></li>
+                    <li class="li_menu"><a href="#" title="trang chủ">Sài Gòn</a></li>
+                    <li class="li_menu"><a href="#" title="trang chủ">Vinh</a></li>
+                    <li class="li_menu"><a href="#" title="trang chủ">Hải Phòng</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h6>VỀ CHÚNG TÔI</h6>
+                <ul class="list-menu">
+                    <li class="li_menu"><a href="#" title="trang chủ">trang chủ</a></li>
+                    <li class="li_menu"><a href="#" title="trang chủ">trang chủ</a></li>
+
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h6>SẢN PHẨM NỔI BẬT</h6>
+                <ul class="list-menu">
+                    <li class="li_menu"><a href="#">Smartphone</a></li>
+                    <li class="li_menu"><a href="#">Máy tính bảng</a></li>
+                    <li class="li_menu"><a href="#">Ti vi</a></li>
+                    <li class="li_menu"><a href="#">Tủ lạnh</a></li>
+                    <li class="li_menu"><a href="#">Âm thanh</a></li>
+                    <li class="li_menu"><a href="#">Mỹ phẩm và làm đẹp</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h6>THEO GIÕI CHÚNG TÔI</h6>
+                <ul class="list-menu">
+                    <li class="li_menu"><a href="#" title="trang chủ">trang chủ</a></li>
+
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-6">
+                <h6>THIÊN ĐƯỜNG MUA SẮM FAMILY MART</h6>
+                <P class="m-0">copyright</P>
+                <P class="m-0">địa chỉ:</P>
+                <P class="m-0">SĐT:</P>
+                <P class="m-0">email:</P>
+            </div>
+            <div class="col-md-6">
+                <h6>NHẬN TIN KHUYẾN MÃI</h6>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="nhập email của bạn." aria-label="Recipient's username" aria-describedby="basic-addon2">
+                    <span class="input-group-text bg-primary text-white" id="">ĐĂNG KÝ</span>
+                </div>
+                <div>
+                    <a href=""><span class="box50 border border-white mr-3 bg-white"><i class="fa-brands fa-facebook-f" style="color: blue;"></i></span></a>
+                    <a href=""><span class="box50 border border-white mr-3 bg-white"><i class="fa-brands fa-twitter" style="color: #4284f5;"></i></span></a>
+                    <a href=""><span class="box50 border border-white mr-3 bg-white"><i class="fa-brands fa-google" style="color: orange;"></i></span></a>
+                    <a href=""><span class="box50 border border-white mr-3 bg-white"><i class="fa-brands fa-youtube " style="color: red;"></i></span></a>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-6">Bản quyền thuộc về </div>
+            <div class="col-md-6 text-end">Câu hỏi thường gặp.</div>
+        </div>
+    </div>
+</section>
+
+<script src="js/bootstrap.bundle.min.js"></script>
